44943d26d0ab16747269fe5bbd4e09202fa79dcb
OwnurD <turhanonurdogucu@gmail.com>
2016-03-10 23:59:26 +0200

    Update api.js
    Added language management for placeholder and value attribute

--- js/api.js
+++ js/api.js
@@ -3,10 +3,16 @@
     var GetData = {
         init : function (data, el) {
             var str = data.label;
-            
+            var split_str = str.split('|');
+            str = split_str[0];
             var jsonData = eval("Content_" + data.language)[str];
+            if (split_str[1] === undefined)
+                el.html(jsonData);
+            else if (split_str[1] == "placeholder")
+                el.attr('placeholder', jsonData);
+            else if (split_str[1] == "value")
+                el.val(jsonData);
             
-            el.html(jsonData);
         }
     };
 
